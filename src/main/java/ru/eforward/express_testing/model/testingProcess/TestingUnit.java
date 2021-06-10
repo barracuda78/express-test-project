40da0c4904296b84b99e56f6ae8ce4a2e09243e9
Andrey Ruzaev <barracuda78@yandex.ru>
2021-06-10 14:08:16 +0300

    TestValidating created

--- src/main/java/ru/eforward/express_testing/model/testingProcess/TestingUnit.java
+++ src/main/java/ru/eforward/express_testing/model/testingProcess/TestingUnit.java
@@ -8,13 +8,17 @@ import ru.eforward.express_testing.utils.LogHelper;
 
 import org.slf4j.Logger;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @Getter
 public class TestingUnit {
     private static final Logger LOGGER = LoggerFactory.getLogger(TestingUnit.class);
 
     private final int lessonId;
     private final int groupId;
-    private volatile String[] questions;
+    private volatile List<String> questions;
     private volatile int cursor;
 
     public TestingUnit(int lessonId, int groupId){
@@ -22,7 +26,7 @@ public class TestingUnit {
         this.groupId = groupId;
         init(lessonId);
         LOGGER.warn("new test was created for lessonId {} and groupId {}", lessonId, groupId);
-        LogHelper.writeMessage("---LogHelper: Внимание! Был создан тест!---");
+        LogHelper.writeMessage("---class TestingUnit method init() : Внимание! Был создан тест!---");
     }
 
     private void init(int lessonId){
@@ -33,34 +37,28 @@ public class TestingUnit {
 //        logger.info("---Just a log message.");
 //        logger.debug("---Message for debug level.");
 //        logger.error("---Failed to read file.");
-        questions = new String[10];
-        questions[0] = array[0];
-        questions[1] = "fdsfdsfds";
-        questions[2] = "блаблабла2";
-        questions[3] = "блаблабла3";
-        questions[4] = "блаблабла4";
-        questions[5] = "блаблабла5";
-        questions[6] = "блаблабла6";
-        questions[7] = "блаблабла7";
-        questions[8] = "блаблабла8";
-        questions[9] = "блаблабла9";
+        questions = new ArrayList<>(Arrays.asList(array));
+        LogHelper.writeMessage("---class TestingUnit method init() : questions = " + questions);
     }
 
     public synchronized boolean hasNextTest(){
-        return questions != null && questions.length >= 1 && cursor < questions.length;
+        return questions != null && questions.size() >= 1 && cursor < questions.size();
     }
 
     public synchronized String getNextTest(){
-        if (questions == null || questions.length < 1 || cursor >= questions.length){
+        if (questions == null || questions.size() < 1 || cursor >= questions.size()){
             return null;
         }
-        String testString = questions[cursor];
-        cursor++;
-        return covertPlainStringToValidHtmlString(testString);
+//        String testString = questions.get(cursor);
+//        if(testString.startsWith("//")){
+//            cursor++;
+//            getNextTest();
+//        }
+//        cursor++;
+        return covertPlainStringToValidHtmlString(questions.get(3));
     }
 
     //        <form method="post" action="">
-    //
     //            <input type="text" required placeholder="login" name="login"><br>
     //            <input type="password" required placeholder="password" name="password"><br><br>
     //            <input class="button" type="submit" value="Войти">
@@ -70,13 +68,34 @@ public class TestingUnit {
         StringBuilder sb = new StringBuilder();
         sb.append("<p>");
         sb.append("<br>");
-        sb.append(plainString);   //------>this is the question itself;
+        sb.append(buildNiceQuestionFromPlainQuestion(plainString));   //------>this is the question itself;
         sb.append("<br>");
         sb.append("<form method=\"get\" action=\"AnswerHandlerServlet\">");  //TODO:------------->have to create this servlet
         sb.append("<input type=\"text\" required placeholder=\"type your answer\" name=\"answer\"><br>");
         sb.append("<input class=\"button\" type=\"submit\" name=\"answeredButton\" value=\"Отправить\">");
         sb.append("</form>");
         sb.append("</p>");
+
+        return sb.toString();
+    }
+
+    //Grant is {~buried =entombed ~living ~затрудняюсь ответить} in Grant's tomb.
+    private synchronized String buildNiceQuestionFromPlainQuestion(String q){
+        LogHelper.writeMessage("---class TestingUnit method  buildNice...() q = " + q);
+        StringBuilder sb = new StringBuilder();
+        String allVariants = q.substring(q.indexOf('{') + 1, q.indexOf('}'));
+        String questionItself = q.substring(0, q.indexOf('{')) + q.substring(q.indexOf('}') + 1);
+
+        sb.append("<b>");
+        sb.append(questionItself);
+        sb.append("</b>");
+        sb.append("<ul>");
+        for(String variant : allVariants.split("[\\s]+")){
+            sb.append("<li>");
+            sb.append(variant);
+            sb.append("</li>");
+        }
+        sb.append("</ul>");
 
         return sb.toString();
     }
